7bfa1240acb7d648c08b0444105eb3b86bda0457
fairlyFossey <188269976+fairlyFossey@users.noreply.github.com>
2026-06-01 18:59:03 -0400

    Add functionality to create and store book objects

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 
 // create empty array
+const library = [];
 
 // create a constructor function for book objects
+function Book(title, author, pageCount, hasFinished) {
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.hasFinished = hasFinished;
+    this.id = crypto.randomUUID();
+}
 
 // create function that creates a new book object from arguments and appends it to the array
+function addBookToLibrary(title, author, pageCount, hasFinished) {
+    let newBook = new Book(title, author, pageCount, hasFinished);
+    library.push(newBook);
+}
+
+addBookToLibrary("The Lord of the Rings", "J.R. Tolkien", 840, false);
+addBookToLibrary("On the Origin of Species", "Charles Darwin", 524, true);
+console.table(library);
 
 // create function that loops through array, displaying all books on the page 
 
