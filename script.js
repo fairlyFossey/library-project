79b7c4409e68f8a30b449707a47518f075190aff
fairlyFossey <188269976+fairlyFossey@users.noreply.github.com>
2026-06-03 22:13:35 -0400

    Refactor displayBooks() for readability

--- script.js
+++ script.js
@@ -19,29 +19,26 @@ function addBookToLibrary(title, author, pageCount, hasFinished) {
 
 addBookToLibrary("The Lord of the Rings", "J.R. Tolkien", 840, false);
 addBookToLibrary("On the Origin of Species", "Charles Darwin", 524, true);
-// console.table(library);
+console.table(library);
 
 // create function that loops through array, displaying all books on the page 
 function displayBooks(source) {
-
-    // create a selector that targets where the books will display
     const bookCollection = document.querySelector(".book-collection-cntr");
 
-    // create a loop that goes through each object in array
     for (const book of source) {
-        // create the divs to be displayed 
+        // create divs to be displayed 
         const card = document.createElement("div");
-        const title = document.createElement("div");
-        const author = document.createElement("div");
-
-        // add classes to the divs plus info from the book object
         card.classList.toggle("book-card");
+
+        const title = document.createElement("div");
         title.classList.toggle("book-title");
         title.textContent = book.title;
+
+        const author = document.createElement("div");
         author.classList.toggle("book-author");
         author.textContent = book.author;
 
-        // append element to parent node on page
+        // append elements to parent nodes
         bookCollection.appendChild(card);
         card.appendChild(title);
         card.appendChild(author);
