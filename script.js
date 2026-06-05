4618720c14f3b38e55ee24c0b6b1def7cecee88d
fairlyFossey <188269976+fairlyFossey@users.noreply.github.com>
2026-06-04 21:25:14 -0400

    Organize JS code structure

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
-// create empty array
 const library = [];
+const bookCollection = document.querySelector(".book-collection-cntr");
 
-// create a constructor function for book objects
+// book constructor
 function Book(title, author, pageCount, hasFinished) {
     this.title = title;
     this.author = author;
@@ -11,44 +10,49 @@ function Book(title, author, pageCount, hasFinished) {
     this.id = crypto.randomUUID();
 }
 
-// create function that creates a new book object from arguments and appends it to the array
+// store books in arr
 function addBookToLibrary(title, author, pageCount, hasFinished) {
     let newBook = new Book(title, author, pageCount, hasFinished);
     library.push(newBook);
 }
 
-addBookToLibrary("The Lord of the Rings", "J.R. Tolkien", 840, false);
-addBookToLibrary("On the Origin of Species", "Charles Darwin", 524, true);
-console.table(library);
 
-// create function that loops through array, displaying all books on the page 
-function displayBooks(source) {
-    const bookCollection = document.querySelector(".book-collection-cntr");
-
-    for (const book of source) {
-        // create divs to be displayed 
-        const card = document.createElement("div");
-        card.classList.toggle("book-card");
-
-        const title = document.createElement("div");
-        title.classList.toggle("book-title");
-        title.textContent = book.title;
-
-        const author = document.createElement("div");
-        author.classList.toggle("book-author");
-        author.textContent = book.author;
-
-        // append elements to parent nodes
-        bookCollection.appendChild(card);
-        card.appendChild(title);
-        card.appendChild(author);
+// display books on page
+function displayBookCollection(arr) {
+    for (const obj of arr) {
+        displayBook(obj)
     }
 }
 
-displayBooks(library);
+function displayBook(obj) {
+    // create divs
+    const card = document.createElement("div");
+    card.classList.toggle("book-card");
 
-// add a button that allows users to add books to the library by filling out a form
+    const title = document.createElement("div");
+    title.classList.toggle("book-title");
+    title.textContent = book.title;
 
-// add a button to each book that can remove it from the library
+    const author = document.createElement("div");
+    author.classList.toggle("book-author");
+    author.textContent = book.author;
 
-// add a button to each book that can toggle its read/unread status
+    // add to dom
+    bookCollection.appendChild(card);
+    card.appendChild(title);
+    card.appendChild(author);
+}
+
+
+// button press opens modal-dialog form 
+
+    // form submission creates new book and displays it
+
+// function to remove book from collection
+
+// function to toggle the read status of books
+
+addBookToLibrary("The Lord of the Rings", "J.R. Tolkien", 840, false);
+addBookToLibrary("On the Origin of Species", "Charles Darwin", 524, true);
+
+displayBookCollection(library);
